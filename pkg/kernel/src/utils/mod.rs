7e2543621cba02b490faf9f6bdc7f59ee19abaaa
ykx3 <yinkaixuan4@gmail.com>
2024-03-25 11:42:36 +0800

    lab2 init

--- pkg/kernel/src/utils/mod.rs
+++ pkg/kernel/src/utils/mod.rs
@@ -6,7 +6,6 @@ pub use macros::*;
 
 pub const fn get_ascii_header() -> &'static str {
     concat!(
-        "\x1B[2J\x1B[1;1H",
         r"
 __  __      __  _____            ____  _____
 \ \/ /___ _/ /_/ ___/___  ____  / __ \/ ___/
@@ -15,7 +14,35 @@ __  __      __  _____            ____  _____
 /_/\__,_/\__//____/\___/_/ /_/\____//____/
 
                                        v",
-        env!("CARGO_PKG_VERSION"),
-        " by 22331116"
+        env!("CARGO_PKG_VERSION")
     )
+}
+
+
+const SHORT_UNITS: [&str; 4] = ["B", "K", "M", "G"];
+const UNITS: [&str; 4] = ["B", "KiB", "MiB", "GiB"];
+
+pub fn humanized_size(size: u64) -> (f32, &'static str) {
+    humanized_size_impl(size, false)
+}
+
+pub fn humanized_size_short(size: u64) -> (f32, &'static str) {
+    humanized_size_impl(size, true)
+}
+
+#[inline]
+pub fn humanized_size_impl(size: u64, short: bool) -> (f32, &'static str) {
+    let bytes = size as f32;
+
+    let units = if short { &SHORT_UNITS } else { &UNITS };
+
+    let mut unit = 0;
+    let mut bytes = bytes;
+
+    while bytes >= 1024f32 && unit < units.len() {
+        bytes /= 1024f32;
+        unit += 1;
+    }
+
+    (bytes, units[unit])
 }
