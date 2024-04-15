d2189c1ea94221426c431369ce0f10e87acb5ba5
ykx3 <yinkaixuan4@gmail.com>
2024-04-15 10:30:14 +0800

    init lab3

--- pkg/kernel/src/main.rs
+++ pkg/kernel/src/main.rs
@@ -3,29 +3,38 @@
 
 use ysos::*;
 use ysos_kernel as ysos;
+
 extern crate alloc;
-use core::arch::asm;
 
 boot::entry_point!(kernel_main);
 
 pub fn kernel_main(boot_info: &'static boot::BootInfo) -> ! {
     ysos::init(boot_info);
-    // unsafe {
-    //     asm!(
-    //         "mov rax, 28",
-    //         "mov [0xDEADBEEF], rax",
-    //         options(nostack)
-    //     );
-    // }
+
+    // FIXME: update lib.rs to pass following tests
+
+    // 1. run some (about 5) "test", show these threads are running concurrently
+
+    // 2. run "stack", create a huge stack, handle page fault properly
+
+    let mut test_num = 0;
+
     loop {
-        print!("> ");
-        let input = input::get_line();
-        match input.trim() {
+        print!("[>] ");
+        let line = input::get_line();
+        match line.trim() {
             "exit" => break,
-            _ => {
-                println!("You said: {}", input);
-                println!("The counter value is {}", interrupt::clock::read_counter());
+            "ps" => {
+                ysos::proc::print_process_list();
             }
+            "stack" => {
+                ysos::new_stack_test_thread();
+            }
+            "test" => {
+                ysos::new_test_thread(format!("{}", test_num).as_str());
+                test_num += 1;
+            }
+            _ => println!("[=] {}", line),
         }
     }
 
--- pkg/kernel/src/utils/mod.rs
+++ pkg/kernel/src/utils/mod.rs
@@ -1,8 +1,16 @@
 #[macro_use]
 mod macros;
+#[macro_use]
+mod regs;
 
+pub mod clock;
+pub mod func;
 pub mod logger;
+
 pub use macros::*;
+pub use regs::*;
+
+use crate::proc::*;
 
 pub const fn get_ascii_header() -> &'static str {
     concat!(
@@ -18,31 +26,38 @@ __  __      __  _____            ____  _____
     )
 }
 
+pub fn new_test_thread(id: &str) -> ProcessId {
+    let proc_data = ProcessData::new();
+    proc_data.set_env("id", id);
 
-const SHORT_UNITS: [&str; 4] = ["B", "K", "M", "G"];
-const UNITS: [&str; 4] = ["B", "KiB", "MiB", "GiB"];
-
-pub fn humanized_size(size: u64) -> (f32, &'static str) {
-    humanized_size_impl(size, false)
+    spawn_kernel_thread(
+        utils::func::test,
+        format!("#{}_test", id),
+        Some(proc_data),
+    )
 }
 
-pub fn humanized_size_short(size: u64) -> (f32, &'static str) {
-    humanized_size_impl(size, true)
+pub fn new_stack_test_thread() {
+    let pid = spawn_kernel_thread(
+        utils::func::stack_test,
+        alloc::string::String::from("stack"),
+        None,
+    );
+
+    // wait for progress exit
+    wait(pid);
 }
 
-#[inline]
-pub fn humanized_size_impl(size: u64, short: bool) -> (f32, &'static str) {
-    let bytes = size as f32;
+fn wait(pid: ProcessId) {
+    loop {
+        // FIXME: try to get the status of the process
 
-    let units = if short { &SHORT_UNITS } else { &UNITS };
+        // HINT: it's better to use the exit code
 
-    let mut unit = 0;
-    let mut bytes = bytes;
-
-    while bytes >= 1024f32 && unit < units.len() {
-        bytes /= 1024f32;
-        unit += 1;
+        if /* FIXME: is the process exited? */ {
+            x86_64::instructions::hlt();
+        } else {
+            break;
+        }
     }
-
-    (bytes, units[unit])
 }
